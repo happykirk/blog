f9f34de916e24292e65eb65fbb1835472eb0de0f
杨珂 <kirkyang@tencent.com>
2020-11-22 17:35:07 +0800

    修改gin-blog为blog

--- main.go
+++ main.go
@@ -1,24 +1,23 @@
 package main
 
 import (
+	"blog/pkg/setting"
+	"blog/routers"
 	"fmt"
-	"gin-blog/pkg/setting"
-	"gin-blog/routers"
 	"log"
 	"net/http"
 )
 
-func main()  {
-	router:=routers.InitRouter()
-	log.Println("port:",setting.HTTPPort)
-	s :=&http.Server{
-		Addr: fmt.Sprintf(":%d",setting.HTTPPort),
-		Handler: router,
-		ReadTimeout: setting.ReadTimeout,
-		WriteTimeout: setting.WriteTimeout,
-		MaxHeaderBytes: 1<<20,
+func main() {
+	router := routers.InitRouter()
+	log.Println("port:", setting.HTTPPort)
+	s := &http.Server{
+		Addr:           fmt.Sprintf(":%d", setting.HTTPPort),
+		Handler:        router,
+		ReadTimeout:    setting.ReadTimeout,
+		WriteTimeout:   setting.WriteTimeout,
+		MaxHeaderBytes: 1 << 20,
 	}
 	s.ListenAndServe()
 
 }
-
--- models/models.go
+++ models/models.go
@@ -1,8 +1,8 @@
 package models
 
 import (
+	"blog/pkg/setting"
 	"fmt"
-	"gin-blog/pkg/setting"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"log"
@@ -10,21 +10,20 @@ import (
 
 var db *gorm.DB
 
-
 type Model struct {
-	ID int `gorm:"primary_key" json:"id"`
-	CreatedOn int `json:"created_on"`
+	ID         int `gorm:"primary_key" json:"id"`
+	CreatedOn  int `json:"created_on"`
 	ModifiedOn int `json:"modified_on"`
 }
 
-func init()  {
+func init() {
 	var (
-		err error
-		dbType,dbName,user,password,host,tablePrefix string
+		err                                               error
+		dbType, dbName, user, password, host, tablePrefix string
 	)
-	sec,err := setting.Cfg.GetSection("database")
-	if err!=nil {
-		log.Fatal(2,"Fail to get section 'database':%v",err)
+	sec, err := setting.Cfg.GetSection("database")
+	if err != nil {
+		log.Fatal(2, "Fail to get section 'database':%v", err)
 	}
 	dbType = sec.Key("TYPE").String()
 	dbName = sec.Key("NAME").String()
@@ -34,28 +33,25 @@ func init()  {
 	tablePrefix = sec.Key("TABLE_PREFIX").String()
 
 	//root:kirk@134217@tcp(127.0.0.1:3306)/blog
-	dbPath:= fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=True&loc=Local",
+	dbPath := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=True&loc=Local",
 		user,
 		password,
 		host,
 		dbName)
 	//fmt.Println("dbpath:",dbPath)
-	db,err = gorm.Open(dbType,dbPath)
+	db, err = gorm.Open(dbType, dbPath)
 
-	if err!=nil{
+	if err != nil {
 		log.Println(err)
 	}
 	gorm.DefaultTableNameHandler = func(db *gorm.DB, defaultTableName string) string {
-		return  tablePrefix + defaultTableName
+		return tablePrefix + defaultTableName
 	}
 	db.SingularTable(true)
 	db.DB().SetMaxIdleConns(10)
 	db.DB().SetMaxOpenConns(100)
 }
 
-
-
-func closeDB()  {
+func closeDB() {
 	defer db.Close()
 }
-
