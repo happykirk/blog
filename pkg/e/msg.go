2600d74e4566abc94ca425f1d344fea113513394
杨珂 <kirkyang@tencent.com>
2021-01-27 19:19:06 +0800

    修改脚本

--- pkg/e/msg.go
+++ pkg/e/msg.go
@@ -11,6 +11,8 @@ var msgFlags = map[int]string{
 	ERROR_AUTH_CHECK_TOKEN_TIMEOUT : "Token已超时",
 	ERROR_AUTH_TOKEN : "Token生成失败",
 	ERROR_AUTH : "Token错误",
+	ERROR_READ_HTTP_BODY_JSON_FAILED:"read http body json failed",
+
 }
 
 func GetMsg(code int) string {
--- routers/api/v1/tag.go
+++ routers/api/v1/tag.go
@@ -5,12 +5,20 @@ import (
 	"blog/pkg/e"
 	"blog/pkg/setting"
 	"blog/pkg/util"
+	"encoding/json"
 	"fmt"
 	"github.com/Unknwon/com"
 	"github.com/astaxie/beego/core/validation"
 	"github.com/gin-gonic/gin"
+	"io/ioutil"
+	"log"
 	"net/http"
 )
+type tagInfo struct {
+	Name      string `json:"name"`
+	State   int    `json:"state"`
+	Created_by   string  `json:"created_by"`
+}
 
 //获取多个文章标签
 func GetTags(c *gin.Context) {
@@ -42,9 +50,52 @@ func GetTags(c *gin.Context) {
 
 //新增文章标签
 func AddTag(c *gin.Context) {
-	name := c.Query("name")
-	state := com.StrTo(c.DefaultQuery("state", "0")).MustInt()
-	createBy := c.Query("created_by")
+
+	var name =""
+	var state  = 0
+	var createBy = ""
+
+	//post json格式，注意json的字符串和数字
+	if c.Request.Header.Get("Content-Type") == "application/json" {
+		body, err := ioutil.ReadAll(c.Request.Body)
+		code := e.INVALID_PARAMS
+		if err != nil {
+			log.Println(err, "read http body failed！error msg:"+err.Error())
+			c.JSON(http.StatusOK, gin.H{
+				"code": code,
+				"msg":  e.GetMsg(code),
+				"data": make(map[string]string),
+			})
+			return
+		}
+		log.Println("INFO|OrderAdd body :" + string(body))
+
+		var params tagInfo
+		err = json.Unmarshal(body, &params)
+		if err != nil {
+			log.Println(err, "read http body json failed! err :"+err.Error())
+			code := e.ERROR_READ_HTTP_BODY_JSON_FAILED
+			c.JSON(http.StatusOK, gin.H{
+				"code": code,
+				"msg":  e.GetMsg(code),
+				"data": make(map[string]string),
+			})
+			return
+		}
+		name = params.Name
+		state = com.StrTo(params.State).MustInt()
+		createBy = params.Created_by
+
+
+	} else {
+		//Query的方式是：http://127.0.0.1:8000/api/v1/tags?name=1&state=1&created_by=test
+		name = c.Query("name")
+		state = com.StrTo(c.DefaultQuery("state", "0")).MustInt()
+		createBy = c.Query("created_by")
+	}
+
+
+
 
 	valid := validation.Validation{}
 	valid.Required(name, "name").Message("名称不能为空")
@@ -143,3 +194,5 @@ func DeleteTag(c *gin.Context) {
 	})
 
 }
+
+
